e44f8ae7d4c6ecc7c7963d17a9388cadeffa9cb8
Anton Lobanov <antnlobanov@gmail.com>
2022-01-21 23:58:24 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from selenium.webdriver import Firefox
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+import csv, os
+from bs4 import BeautifulSoup
+from urllib.request import urlretrieve
+
+options = Options()
+
+f = open('СписокНоменклатур.csv', 'r', encoding='utf-8')
+reader = csv.reader(f, delimiter=';')
+
+for row in list(reader)[1:3]:
+    os.chdir('/home/anton/Документы/imgparse')
+    driver = Firefox(firefox_binary = FirefoxBinary('/usr/bin/firefox'), executable_path=os.getcwd() + '/geckodriver', options = options)
+    driver.get('https://yandex.ru/images/search?text=' + row[0])
+    soup = BeautifulSoup(driver.page_source, features='html5lib')
+    driver.quit()
+    img = 'https:' + soup.find('img', {"class": "serp-item__thumb justifier__thumb"}, src = True)['src']
+    os.mkdir(row[1])
+    os.chdir(row[1])
+    urlretrieve(img, row[1] + '.png')
