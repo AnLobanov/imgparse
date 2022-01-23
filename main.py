217793fbe582c4d5c801c3cbdf77c2493e501686
BuildTools <unconfigured@null.spigotmc.org>
2022-01-23 13:14:11 +0500

    Added comments

--- main.py
+++ main.py
@@ -5,25 +5,37 @@ import csv, os
 from bs4 import BeautifulSoup
 from urllib.request import urlretrieve
 
+# Отключаем GUI браузера
+
 options = Options()
 options.headless = True
 
 rootdir = os.getcwd()
 
+# Подключаемся к файлу в формате CSV с разделителем ;
+
 f = open('СписокНоменклатур.csv', 'r', encoding='utf-8')
 reader = csv.reader(f, delimiter=';')
+
+# Если папки нет - создаем
 
 if not os.path.exists('imgs'):
     os.mkdir('imgs')
 
-for row in list(reader)[1:]:
+for row in list(reader)[1:26]:
     os.chdir(rootdir)
+
+    # Загружаем страницу и ее код
+
     driver = Firefox(firefox_binary = FirefoxBinary('/usr/bin/firefox'), executable_path=os.getcwd() + '/geckodriver', options = options)
     driver.get('https://yandex.ru/images/search?text=' + row[0])
     soup = BeautifulSoup(driver.page_source, features='html5lib')
     driver.quit()
     img = 'https:' + soup.find('img', {"class": "serp-item__thumb justifier__thumb"}, src = True)['src']
     os.chdir('imgs')
+
+    # Скачиваем 1 изображение из Яндекс.Картинок в папку с артикулом
+
     if not os.path.exists(row[1]):
         os.mkdir(row[1])
         os.chdir(row[1])
