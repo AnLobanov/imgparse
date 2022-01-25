cae18ba4de8d64b2b571bb60d736e6ed34f1acc3
Anton <antnlobanov@gmail.com>
2022-01-25 16:42:29 +0500

    dont parse existing file

--- main.py
+++ main.py
@@ -27,34 +27,37 @@ delay = random.randint(2, 7)
 
 queue = 0
 for row in list(reader)[1:]:
-    os.chdir(rootdir)
+    if not os.path.isfile(rootdir + '/imgs/' + row[1] + '/' + row[1] + str(por) + '.png'):
+        os.chdir(rootdir)
 
-    # Загружаем страницу и ее код
-    try:
-        driver = Firefox(executable_path=os.getcwd() + '/geckodriver', options = options)
-        driver.get('https://yandex.ru/images/search?text=' + row[0])
-        soup = BeautifulSoup(driver.page_source, features='html5lib')
-        driver.quit()
-        img = 'https:' + soup.findAll('img', {"class": "serp-item__thumb justifier__thumb"}, src = True)[por]['src']
-    except IndexError:
-        time.sleep(random.uniform(10, 20))
-        driver = Firefox(executable_path=os.getcwd() + '/geckodriver', options = options)
-        driver.get('https://yandex.ru/images/search?text=' + row[0])
-        soup = BeautifulSoup(driver.page_source, features='html5lib')
-        driver.quit()
-        img = 'https:' + soup.findAll('img', {"class": "serp-item__thumb justifier__thumb"}, src = True)[por]['src']
-    os.chdir('imgs')
+        # Загружаем страницу и ее код
+        try:
+            driver = Firefox(executable_path=os.getcwd() + '/geckodriver', options = options)
+            driver.get('https://yandex.ru/images/search?text=' + row[0])
+            soup = BeautifulSoup(driver.page_source, features='html5lib')
+            driver.quit()
+            img = 'https:' + soup.findAll('img', {"class": "serp-item__thumb justifier__thumb"}, src = True)[por - 1]['src']
+        except IndexError:
+            time.sleep(random.uniform(10, 20))
+            driver = Firefox(executable_path=os.getcwd() + '/geckodriver', options = options)
+            driver.get('https://yandex.ru/images/search?text=' + row[0])
+            soup = BeautifulSoup(driver.page_source, features='html5lib')
+            driver.quit()
+            img = 'https:' + soup.findAll('img', {"class": "serp-item__thumb justifier__thumb"}, src = True)[por - 1]['src']
+        os.chdir('imgs')
 
-    # Скачиваем 1 изображение из Яндекс.Картинок в папку с артикулом
+        # Скачиваем 1 изображение из Яндекс.Картинок в папку с артикулом
 
-    if not os.path.exists(row[1]):
-        os.mkdir(row[1])
-        os.chdir(row[1])
-        urlretrieve(img, row[1] + por + '.png')
+        if not os.path.exists(row[1]):
+            os.mkdir(row[1])
+            os.chdir(row[1])
+            urlretrieve(img, row[1] + str(por) + '.png')
 
-    # Ждем следующий запрос
+        # Ждем следующий запрос
 
-    queue += 1
-    if queue == delay:
-        time.sleep(random.uniform(30, 120))
-        delay = random.randint(2, 7)
+        queue += 1
+        if queue == delay:
+            time.sleep(random.uniform(30, 120))
+            delay = random.randint(2, 7)
+    else:
+        print(row[1])
